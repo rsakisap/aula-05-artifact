6331844c17d1156d9c9576deff65f9a4948eda2e
rsakisap <raphael.pessoa@aluno.faculdadeimpacta.com.br>
2024-09-10 22:54:36 +0000

    commit4

--- hello-server.go
+++ hello-server.go
@@ -1,13 +1,12 @@
- package main
- import (
- "fmt"
- "net/http"
- )
- func main() {
- }
- http.HandleFunc("/", HelloServer)
- http.ListenAndServe(":13000", nil)
- func HelloServer(w http.ResponseWriter, r 
-*http.Request) {
- fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
- }
+package main
+import (
+"fmt"
+"net/http"
+)
+func main() {
+}
+http.HandleFunc("/", HelloServer)
+http.ListenAndServe(":13000", nil)
+func HelloServer(w http.ResponseWriter, http.Request) {
+fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
